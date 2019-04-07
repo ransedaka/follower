e71f10ee62ea001968226045e1e8d26d77f6afef
RanS <ran.sedaka@harman.com>
2019-04-07 21:00:51 +0300

    fix page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './../../services/users.service';
 import { Observable } from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+import {NavController} from "@ionic/angular";
 
 
 @Component({
@@ -10,12 +12,18 @@ import { Observable } from 'rxjs';
 })
 export class ProfilePage implements OnInit {
 
-  results: Observable<any>;
+  users: Observable<any>;
 
-  constructor(private usersService: UsersService) { }
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.users= this.http.get('http://ec2-34-243-7-84.eu-west-1.compute.amazonaws.com:8080/v1/users')
+    this.users.subscribe(data => {console.log('my data: ', data);})
+  }
+
+  //constructor(private usersService: UsersService) { }
 
   ngOnInit() {
-    this.results = this.usersService.getUsers();
+    //this.users = this.usersService.getUsers();
   }
+
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,11 +21,14 @@ export class UsersService {
    * @param {string} title Search Term
    * @returns Observable with the search results
    */
-  getUsers(): Observable<any> {
-    return this.http.get('ec2-34-243-7-84.eu-west-1.compute.amazonaws.com:8080/v1/users').pipe(
-        map(results => results['users'])
-    );
-  }
+  /*getUsers(): Observable<any> {
 
+    return this.http.get('http://ec2-34-243-7-84.eu-west-1.compute.amazonaws.com:8080/v1/users').pipe(
+        map(results => results['users']));
+  }*/
+
+  openDetails(user) {
+
+  }
 
 }
